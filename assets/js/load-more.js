811b9cc07549b3126fc6019cbf5d05beb2be1504
Dmitry <volcharo@gmail.com>
2023-08-01 10:50:42 +0300

    code

--- assets/js/load-more.js
+++ assets/js/load-more.js
@@ -1,33 +1,56 @@
+
 // Load more galleries
-jQuery(document).ready(function($) {
-	let galleries = $('div.gallery')
-	let galleriesPerPage = 3
-	let currentPage = 1
-	let totalGalleries = galleries.length
-	let totalPages = Math.ceil(totalGalleries / galleriesPerPage)
 
-	const isLastPageCheck = () => {
-		if (currentPage === totalPages) {
-			$('#load-more').hide()
-		}
-	}
-
-	$('#load-more').on('click', e => {
-		e.preventDefault()
-
-		if (currentPage < totalPages) {
-			let startIndex = currentPage * galleriesPerPage
-			let endIndex = startIndex + galleriesPerPage
-
-			$('div.gallery').slice(startIndex, endIndex).fadeIn()
-
-			currentPage++
-		}
-
-		isLastPageCheck()
+// Hide galleries at start
+$(document).ready (() => {
+	
+	// $('.gallery.hidden').hide();
+	
+	// Open gallery set
+	$('button[gallery-set-id]').on('click', function (){
+		console.log(1);
+		const gallerySetId = $(this).attr('gallery-set-id');
+		console.log(gallerySetId)
+		
+		$(`.gallery[gallery-set-id=${gallerySetId}]`).removeClass('hidden');
+		$(this).hide();
 	})
 
-	galleries.slice(galleriesPerPage).hide()
-
-	isLastPageCheck()
 })
+
+
+// Load more galleries
+// jQuery(document).ready(function($) {
+// 	let galleries = $('div.gallery')
+// 	let galleriesPerPage = 3
+// 	let currentPage = 1
+// 	let totalGalleries = galleries.length
+// 	let totalPages = Math.ceil(totalGalleries / galleriesPerPage)
+
+// 	const isLastPageCheck = () => {
+// 		if (currentPage === totalPages) {
+// 			$('#load-more').hide()
+// 		}
+// 	}
+
+// 	$('#load-more').on('click', e => {
+// 		e.preventDefault()
+
+// 		console.log (111)
+
+// 		if (currentPage < totalPages) {
+// 			let startIndex = currentPage * galleriesPerPage
+// 			let endIndex = startIndex + galleriesPerPage
+
+// 			$('div.gallery').slice(startIndex, endIndex).fadeIn()
+
+// 			currentPage++
+// 		}
+
+// 		isLastPageCheck()
+// 	})
+
+// 	galleries.slice(galleriesPerPage).hide()
+
+// 	isLastPageCheck()
+// })
